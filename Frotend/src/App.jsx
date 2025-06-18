384d6f0036167e9db7b03d1e743a9eadf8a645f1
Jean Bosco B <jeanboscob77@gmail.com>
2025-06-18 14:20:28 +0200

    live

--- Frotend/src/App.jsx
+++ Frotend/src/App.jsx
@@ -1,11 +1,11 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { FaEdit } from 'react-icons/fa';
-import { FaTrash } from 'react-icons/fa6';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { FaEdit } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa6";
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
-  const [task, setTask] = useState('');
+  const [task, setTask] = useState("");
   const [name, setName] = useState(false);
   const [completed, setCompleted] = useState(false);
   const [loading, setLoading] = useState(true);
@@ -14,7 +14,9 @@ const App = () => {
   useEffect(() => {
     const fetchTasks = async () => {
       try {
-        const res = await axios.get('http://localhost:5000/api/tasks');
+        const res = await axios.get(
+          "https://task-manager-vh8t.onrender.com/api/tasks"
+        );
         setTasks(res.data);
       } catch (error) {
         console.error("Error fetching tasks:", error);
@@ -28,9 +30,15 @@ const App = () => {
   const handleAddTask = async () => {
     if (task.trim()) {
       try {
-        const res = await axios.post('http://localhost:5000/api/tasks', { task, completed });
+        const res = await axios.post(
+          "https://task-manager-vh8t.onrender.com/api/tasks",
+          {
+            task,
+            completed,
+          }
+        );
         setTasks((prevTasks) => [...prevTasks, res.data]);
-        setTask('');
+        setTask("");
       } catch (error) {
         console.error("Error adding task:", error);
       }
@@ -39,14 +47,24 @@ const App = () => {
 
   const editTaskHandler = async () => {
     try {
-      const res = await axios.put(`http://localhost:5000/api/tasks/${id}`, { task, completed });
-      setTasks(tasks.map((item) => (item._id === id ? { ...item, task: res.data.task } : item)));
+      const res = await axios.put(
+        `https://task-manager-vh8t.onrender.com/api/tasks/${id}`,
+        {
+          task,
+          completed,
+        }
+      );
+      setTasks(
+        tasks.map((item) =>
+          item._id === id ? { ...item, task: res.data.task } : item
+        )
+      );
     } catch (error) {
       console.error("Error editing task:", error);
     } finally {
       setName(false);
       setId(null);
-      setTask('');
+      setTask("");
     }
   };
 
@@ -64,7 +82,9 @@ const App = () => {
 
   const handleDeleteTask = async (id) => {
     try {
-      await axios.delete(`http://localhost:5000/api/tasks/${id}`);
+      await axios.delete(
+        `https://task-manager-vh8t.onrender.com/api/tasks/${id}`
+      );
       setTasks(tasks.filter((item) => item._id !== id));
     } catch (error) {
       console.error("Error deleting task:", error);
@@ -78,30 +98,33 @@ const App = () => {
     setTasks(updatedTasks);
     const updatedTask = updatedTasks.find((item) => item._id === id);
     try {
-      await axios.put(`http://localhost:5000/api/tasks/${id}`, {
-        task: updatedTask.task,
-        completed: updatedTask.completed,
-      });
+      await axios.put(
+        `https://task-manager-vh8t.onrender.com/api/tasks/${id}`,
+        {
+          task: updatedTask.task,
+          completed: updatedTask.completed,
+        }
+      );
     } catch (error) {
       console.error("Error updating task status:", error);
     }
   };
 
   return (
-    <div className='max-w-md mx-auto mt-10'>
-      <h1 className='font-bold text-2xl mb-4'>Task Manager</h1>
-      <div className='flex mb-4'>
+    <div className="max-w-md mx-auto mt-10">
+      <h1 className="font-bold text-2xl mb-4">Task Manager</h1>
+      <div className="flex mb-4">
         <input
-          type='text'
-          className='border flex-grow p-2'
+          type="text"
+          className="border flex-grow p-2"
           value={task}
           onChange={(e) => setTask(e.target.value)}
         />
         <button
-          className='bg-blue-500 ml-2 text-xl text-white p-2 rounded'
+          className="bg-blue-500 ml-2 text-xl text-white p-2 rounded"
           onClick={MainHandler}
         >
-          {!name ? 'Add' : 'Update'}
+          {!name ? "Add" : "Update"}
         </button>
       </div>
       <div>
@@ -109,24 +132,30 @@ const App = () => {
           <p>Loading data...</p>
         ) : (
           tasks.map((task) => (
-            <ul className='list-none' key={task._id}>
-              <li className='flex items-center mb-4'>
+            <ul className="list-none" key={task._id}>
+              <li className="flex items-center mb-4">
                 <input
-                  type='checkbox'
-                  className='mr-2'
+                  type="checkbox"
+                  className="mr-2"
                   checked={task.completed}
                   onChange={() => handleChecked(task._id)}
                 />
-                <span className={task.completed ? 'flex-grow line-through decoration-red-600' : 'flex-grow'}>
+                <span
+                  className={
+                    task.completed
+                      ? "flex-grow line-through decoration-red-600"
+                      : "flex-grow"
+                  }
+                >
                   {task.task}
                 </span>
-                <div className='flex'>
+                <div className="flex">
                   <FaEdit
-                    className='cursor-pointer text-blue-500 mr-2'
+                    className="cursor-pointer text-blue-500 mr-2"
                     onClick={() => handleEditTask(task._id)}
                   />
                   <FaTrash
-                    className='cursor-pointer text-red-500'
+                    className="cursor-pointer text-red-500"
                     onClick={() => handleDeleteTask(task._id)}
                   />
                 </div>
